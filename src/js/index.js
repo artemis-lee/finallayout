a0d2e7c5d239bd442c6680a757d18b19f6ea7418
Artem <artem.23.cz@gmail.com>
2025-01-21 19:39:26 +0900

    feat: tablet

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,80 @@
 import '../scss/style.scss';
-import '../scss/fonts.scss';
 
+// popup variables
 const openButtonFeedback = document.querySelector('.header__button-feedback');
 const openButtonCall = document.querySelector('.header__button-call');
 const closeButtonFeedback = document.querySelector('.popup__feedback-close');
 const closeButtonCall = document.querySelector('.popup__call-close');
 const pickFeedback = document.querySelector('.popup__feedback');
 const pickCall = document.querySelector('.popup__call');
+
+// sidebar toggle variables
 const background = document.querySelector('.section');
-const noAside = document.querySelector('.content')
+const noAside = document.querySelector('.content');
+const sidebar = document.querySelector('.sidebar');
+const sidebarToggle = document.querySelector('.sidebar-toggle');
 
-openButtonFeedback.addEventListener('click', () => {
-  pickFeedback.classList.toggle('show');
-  background.classList.toggle('blurred');
-});
+// readmore button variables
+const hiddenText = document.querySelector('.description__hidden-text');
+const readmoreButton = document.querySelector('.description__readmore');
+const readmoreIcon = document.querySelector('.readmore-icon')
 
+// hidden brands items variables
+const buttonText = document.querySelector('.btn-text');
+const iconBtn = document.querySelector('.icon');
+const hiddenItems = document.querySelectorAll('.brands-menu__cards__hidden');
+const toggleButton = document.querySelector('.brands-menu__show-all');
 
-openButtonCall.addEventListener('click', () => {
-  pickCall.classList.toggle('show');
-  background.classList.toggle('blurred');
-});
+// hidden device items variables
+const buttonDeviceText = document.querySelector('.device-btn-text');
+const iconDeviceBtn = document.querySelector('.device-icon');
+const hiddenDeviceItems = document.querySelectorAll('.device-menu__cards-hidden');
+const toggleDeviceButton = document.querySelector('.device-menu__show-all');
 
-closeButtonCall.addEventListener('click', () => {
-  pickCall.classList.remove('show');
+// popup 
+function blurBackground(popup) {
+  popup.classList.toggle('show');
+  background.classList.toggle('blurred')
+}
+function closePopup (popup) {
+  popup.classList.remove('show');
   background.classList.remove('blurred');
-});
+}
+openButtonFeedback.addEventListener('click', () => blurBackground (pickFeedback));
+openButtonCall.addEventListener('click', () => blurBackground (pickCall));
 
-closeButtonFeedback.addEventListener('click', () => {
-  pickFeedback.classList.remove('show');
-  background.classList.remove('blurred');
-});
 
-  const hiddenItems = document.querySelectorAll('.brands-menu__cards__hidden');
-  const buttonText = document.querySelector('.btn-text');
-  const iconBtn = document.querySelector('.icon');
-  const toggleButton = document.querySelector('.brands-menu__show-all');
+closeButtonCall.addEventListener('click', () => closePopup (pickCall));
+closeButtonFeedback.addEventListener('click', () => closePopup (pickFeedback));
 
-  toggleButton.addEventListener('click', () => {
+
+// hidden items
+ toggleButton.addEventListener('click', () => {
     buttonText.innerText =
       buttonText.innerText === 'Скрыть' ? 'Показать все' : 'Скрыть';
-
     hiddenItems.forEach((item) => {
       item.style.display =
         item.style.display === 'none' || item.style.display === ''
           ? 'block'
           : 'none';
     });
-
     iconBtn.classList.toggle('closed');
   });
 ;
 
-const sidebar = document.querySelector('.sidebar');
-const sidebarToggle = document.querySelector('.sidebar-toggle');
+// hidden device items
+toggleDeviceButton.addEventListener('click', () => {
+  buttonDeviceText.innerText =
+    buttonDeviceText.innerText === 'Скрыть' ? 'Показать все' : 'Скрыть';
+  hiddenDeviceItems.forEach((item) => {
+    item.style.display =
+      item.style.display === 'none' || item.style.display === ''
+        ? 'block'
+        : 'none';
+  });
+  iconBtn.classList.toggle('closed');
+});
+
 
 if (sidebarToggle) {
   sidebarToggle.addEventListener('click', () => {
@@ -61,14 +83,14 @@ if (sidebarToggle) {
   });
 }
 
-const hiddenText = document.querySelector('.description__hidden-text');
-const readmoreButton = document.querySelector('.description__readmore');
+// readmore
 
 readmoreButton.addEventListener('click', () => {
   hiddenText.classList.toggle('hidden');
   if (hiddenText.classList.contains('hidden')) {
-    readmoreButton.textContent = 'Скрыть'; 
+    readmoreButton.textContent = 'Скрыть';
   } else {
-    readmoreButton.textContent = 'Читать далее'; 
+    readmoreButton.textContent = 'Читать далее';
+    readmoreIcon.classList.toggle('closed');
   }
 });
