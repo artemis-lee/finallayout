12569c90b7f957d542d16d00d65253f6ae454682
Artem <artem.23.cz@gmail.com>
2025-01-23 00:15:42 +0900

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -95,3 +95,14 @@ readmoreButton.addEventListener('click', () => {
   }
 });
 
+new Swiper (".brands__slider", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: "swiper-button-next",
+    prevEl: "swiper-button-prev",
+  },
+  pagination: {
+    el: "swiper-pagination",
+    clickable: true,
+  },
+})
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,11 @@
 new Swiper (".brands__slider", {
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: "swiper-button-prev",
-  //   },
+  slidesPerView: 1,
+    navigation: {
+      nextEl: "swiper-button-next",
+      prevEl: "swiper-button-prev",
+    },
     pagination: {
-      el: ".swiper-pagination",
+      el: "swiper-pagination",
       clickable: true,
     },
   })
